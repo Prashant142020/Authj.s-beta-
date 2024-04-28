fe97efad6e7c8cfc8df2fa0aa27a22241f5f54c9
Prashant142020 <prashant142020@gmail.com>
2024-04-29 00:56:34 +0530

    fix : trustHost in auth.ts

--- auth.ts
+++ auth.ts
@@ -1,16 +1,15 @@
-import NextAuth from "next-auth"
-import github from "next-auth/providers/github"
-import google from "next-auth/providers/google"
-import { PrismaAdapter } from "@auth/prisma-adapter"
-import prisma from "./lib/prisma"
-import { Adapter } from "next-auth/adapters"
- 
+import NextAuth from 'next-auth';
+import github from 'next-auth/providers/github';
+import google from 'next-auth/providers/google';
+import { PrismaAdapter } from '@auth/prisma-adapter';
+import prisma from './lib/prisma';
+import { Adapter } from 'next-auth/adapters';
 
- 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  theme:{
-    logo: "/logo.svg",
+  trustHost: true,
+  theme: {
+    logo: '/logo.svg',
   },
   adapter: PrismaAdapter(prisma) as Adapter,
-  providers: [google ,github],
-})
+  providers: [google, github],
+});
